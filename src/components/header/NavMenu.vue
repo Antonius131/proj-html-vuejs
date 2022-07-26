19c65a7c914555bd561f3a55786aea92b6d72468
Antonius131 <antomendetta@gmail.com>
2022-07-26 14:55:18 +0200

    NavMenu: creating dynamic NavMenu

--- src/components/header/NavMenu.vue
+++ src/components/header/NavMenu.vue
@@ -1,12 +1,30 @@
 <template>
    <div class="nav-menu">
-      
+      <ul>
+         <li 
+            v-for="item in navMenuItems"
+            :key="item"
+         >
+         {{ item }}   
+         </li>
+      </ul>
    </div>
 </template>
 
 <script>
 export default {
-
+   data: function() {
+      return {
+         navMenuItems: {
+            _1stItem: 'Home',
+            _2ndtItem: 'Pages',
+            _3rdItem: 'Courses',
+            _4thItem: 'Features',
+            _5thItem: 'Blog',
+            _6thItem: 'Shop'
+         }
+      }
+   }
 }
 </script>
 
