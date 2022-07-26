34893f4a94777b4bb58e3a1e1af64242af7014d4
Antonius131 <antomendetta@gmail.com>
2022-07-26 17:50:01 +0200

    MainHeader: importing jumbotron in template

--- src/components/header/MainHeader.vue
+++ src/components/header/MainHeader.vue
@@ -1,15 +1,18 @@
 <template>
    <header class="container-lg">
       <NavBar />
+      <MainJumbotron />
    </header>
 </template>
 
 <script>
 import NavBar from './NavBar.vue';
+import MainJumbotron from './MainJumbotron.vue';
 
 export default {
    components: {
-    NavBar
+      NavBar,
+      MainJumbotron
    }
 }
 </script>
