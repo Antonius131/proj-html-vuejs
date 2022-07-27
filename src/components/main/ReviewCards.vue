80665db15e2d6cdf9872e041e0843ef9aa5107f7
Antonius131 <antomendetta@gmail.com>
2022-07-27 22:15:53 +0200

    ReviewCards: creating component layout

--- src/components/main/ReviewCards.vue
+++ src/components/main/ReviewCards.vue
@@ -1,6 +1,13 @@
 <template>
    <div class="row">
-
+      <div class="col-4">
+         <div class="card">
+            <div class="card-body">
+               <h5 class="card-title">Special title treatment</h5>
+               <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+         </div>
+      </div>
    </div>
 </template>
 
