9ebcf355f0eb5a803564df84e1be072f44a0539c
Antonius131 <antomendetta@gmail.com>
2022-07-27 10:11:13 +0200

    BrandCards: creating objects array

--- src/components/main/BrandCards.vue
+++ src/components/main/BrandCards.vue
@@ -20,7 +20,36 @@
 
 <script>
 export default {
-   
+   data: function() {
+      return {
+         brandCards: [
+            {
+               title: 'One to One',
+               subtitle: 'Getting the necessary clarity about the current state to help you improve your game.'
+            },
+            {
+               title: 'Anywhere',
+               subtitle: 'Access to valuable and portable program which allow you to setup and live anywhere you want.'
+            },
+            {
+               title: 'On time',
+               subtitle: 'Punctuality is our top priority because it\'s an essential criteria to assess a program quality.'
+            },
+            {
+               title: 'Online Courses',
+               subtitle: 'Online business coaching now offers you a very powerful way to empower your business into success.'
+            },
+            {
+               title: 'Consulting',
+               subtitle: 'You will get a clear sense of direction for your business, thorough assessment and faster results.'
+            },
+            {
+               title: 'Self Development',
+               subtitle: 'Business Coaching often keep your focus and develop you both in a professional and personal way.'
+            },
+         ]
+      }
+   }
 }
 </script>
 
