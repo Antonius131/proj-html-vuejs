88b002747b704f61c28e3126cdab4d05398753e0
Antonius131 <antomendetta@gmail.com>
2022-07-27 16:47:35 +0200

    CourseCards: dynamically adding course-caption

--- src/components/main/CourseCards.vue
+++ src/components/main/CourseCards.vue
@@ -11,6 +11,17 @@
                <div class="card-body text-white">
                   <h6 class="card-title">{{ course.price }}</h6>
                   <p class="card-text">{{ course.text }}</p>
+                  <div class="course-caption">
+                     <span>
+                        {{ course.lessons }}
+                     </span>
+                     <span>
+                        {{ course.students }}
+                     </span>
+                     <p>
+                        {{ course.purpose }}
+                     </p>
+                  </div>
                </div>
             </div>
          </div>
@@ -26,22 +37,34 @@ export default {
             {
                imgPath: require('/src/assets/img/courses/course-02-443x600.jpg'),
                price: '$40.00',
-               text: 'Learning to Write as a Professional Author'
+               text: 'Learning to Write as a Professional Author',
+               lessons: '20 Lessons',
+               students: '50 Students',
+               purpose: 'The purpose of this course is to provide the for inspiring readers and...'
             },
             {
                imgPath: require('/src/assets/img/courses/stock-full-hd-03-443x600.jpg'),
                price: 'Free',
-               text: 'Customer-centric Info-Tech Strategies'
+               text: 'Customer-centric Info-Tech Strategies',
+               lessons: '20 Lessons',
+               students: '50 Students',
+               purpose: 'The purpose of this course is to provide the for inspiring readers and...'
             },
             {
                imgPath: require('/src/assets/img/courses/stock-full-hd-04-443x600.jpg'),
                price: '$19.00',
-               text: 'Open Programming Courses for Everyone: Python'
+               text: 'Open Programming Courses for Everyone: Python',
+               lessons: '20 Lessons',
+               students: '50 Students',
+               purpose: 'The purpose of this course is to provide the for inspiring readers and...'
             },
             {
                imgPath: require('/src/assets/img/courses/stock-full-hd-06-443x600.jpg'),
                price: '$26.00',
-               text: 'Academic Listening and Note-taking'
+               text: 'Academic Listening and Note-taking',
+               lessons: '20 Lessons',
+               students: '50 Students',
+               purpose: 'The purpose of this course is to provide the for inspiring readers and...'
             }
          ]
       }
@@ -68,7 +91,6 @@ export default {
 
                h6,
                p {
-                  font-size: $nav_items;
                   font-weight: 800;
                }
 
