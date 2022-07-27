975ba6e968d12a0a5c77651cbaef0a5da5056cd7
Antonius131 <antomendetta@gmail.com>
2022-07-27 17:42:07 +0200

    InfoTabs: creating infoTabs objects array

--- src/components/main/InfoTabs.vue
+++ src/components/main/InfoTabs.vue
@@ -17,7 +17,24 @@
 
 <script>
 export default {
-
+   data: function() {
+      return {
+         infoTabs: [
+            {
+               titleItem: "How can we help?",
+               body: "How many courses you've been on, using however many techniques and methods. Unless you've come to redlize the value of mindset, then you've yet to understand how success comes. With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success."
+            },
+            {
+               titleItem: "Why would I need a Business COACH?",
+               body: "How many courses you've been on, using however many techniques and methods. Unless you've come to redlize the value of mindset, then you've yet to understand how success comes. With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success."
+            },
+            {
+               titleItem: "What is one-on-one coaching?",
+               body: "How many courses you've been on, using however many techniques and methods. Unless you've come to redlize the value of mindset, then you've yet to understand how success comes. With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success."
+            }
+         ]
+      }
+   }
 }
 </script>
 
