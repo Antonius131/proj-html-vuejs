8de168e0b33d74ce4f4b45889e8c5d1996e2c90c
Antonius131 <antomendetta@gmail.com>
2022-07-27 10:58:10 +0200

    SectionTitle: creating component

---
+++ src/components/main/SectionTitle.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="row">
+      <div class="col-12 text-center section-title">
+         <h4>subtitle</h4>
+         <h3>title</h3>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
