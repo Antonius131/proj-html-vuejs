fcf3156a6e42f0583a9d02941b05b640f60f1fe8
Antonius131 <antomendetta@gmail.com>
2022-07-27 14:54:04 +0200

    CoachCards: styling component

--- src/components/main/CoachCards.vue
+++ src/components/main/CoachCards.vue
@@ -6,7 +6,7 @@
          :key="index"
       >
          <div class="card">
-            <img :src="coach.imgPath" class="card-img-top" alt="card-image">
+            <img :src="coach.imgPath" class="card-img-top" alt="coach-image">
             <div class="card-body text-center">
                <h5 class="card-title">
                   {{ coach.title }}
@@ -15,9 +15,7 @@
                   {{ coach.text }}
                </p>
             </div>
-            <div class="link-box text-center">
-               <a href="#" class="btn go-to-btn">GoTo btn</a>
-            </div>
+            <a href="#" class="go-to-btn text-center rounded-bottom">Discover now</a>
          </div>
       </div>
    </div>
@@ -49,6 +47,36 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '/src/assets/css/variables';
 
+   .card {
+      border-color: $border_color;
+      box-shadow: $box_shadow;
+   }
+
+   .card-body {
+      padding: 2rem 2.5rem;
+
+      .card-text {
+         font-size: $nav_items;
+         color: $text_primary_rgba;
+      }
+
+   }
+
+   .go-to-btn {
+      border-top: 1px solid $border_color;
+      padding: .75rem 0;
+      font-size: $nav_items;
+      color: $brand_secondary;
+      font-weight: 800;
+      text-decoration: none;
+      
+      &:hover {
+         background-color: $brand_secondary;
+         cursor: pointer;
+         color: $text_secondary;
+      }
+   }
 </style>
