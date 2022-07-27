6a465ad471b9dacd936ee421af2a3fbb77e9946f
Antonius131 <antomendetta@gmail.com>
2022-07-27 22:27:35 +0200

    ReviewsCards: dynamically creating cards

--- src/components/main/ReviewCards.vue
+++ src/components/main/ReviewCards.vue
@@ -1,14 +1,21 @@
 <template>
    <div class="row">
       <div class="col-4">
-         <div class="card">
+         <div 
+            class="card"
+            v-for="(review, index) in reviewCards"
+            :key="index"
+         >
             <div class="card-body">
-               <h5 class="card-title">title</h5>
-               <p class="card-text">text</p>
+               <h5 class="card-title">{{ rieview.title }}</h5>
+               <p class="card-text">{{ review.text }}</p>
                <div class="user-review-box">
-                  <img class="rounded-circle" src="" alt="">
-                  <h6 class="card-title">Name</h6>
-                  <p><small>job</small></p>
+                  <img 
+                     class="rounded-circle" 
+                     :src="review.imgPath" 
+                     alt="review-img">
+                  <h6 class="card-title">{{ review.name }}</h6>
+                  <p><small>{{ review.job }}</small></p>
                </div>
             </div>
          </div>
