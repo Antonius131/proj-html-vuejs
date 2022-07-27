bfb4b6076a1d9bf8878acc36cf3b53e82b9a452d
Antonius131 <antomendetta@gmail.com>
2022-07-27 17:37:26 +0200

    InfoTabs: creating template layout

--- src/components/main/InfoTabs.vue
+++ src/components/main/InfoTabs.vue
@@ -1,5 +1,18 @@
 <template>
-   <div></div>
+   <div class="accordion">
+      <div class="accordion-item">
+         <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+               Accordion Item 
+            </button>
+         </h2>
+         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+               text
+            </div>
+         </div>
+      </div>
+</div>
 </template>
 
 <script>
