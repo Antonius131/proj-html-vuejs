d60360f13fde47d553c73c603b976a4c4889976f
Antonius131 <antomendetta@gmail.com>
2022-07-27 17:44:04 +0200

    InfoTabs: dynamically creating accordion-items

--- src/components/main/InfoTabs.vue
+++ src/components/main/InfoTabs.vue
@@ -1,14 +1,18 @@
 <template>
    <div class="accordion">
-      <div class="accordion-item">
+      <div 
+         class="accordion-item"
+         v-for="(accordion, index) in infoTabs"
+         :key="index"
+      >
          <h2 class="accordion-header" id="headingOne">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-               Accordion Item 
+               {{ accordion.title }}
             </button>
          </h2>
          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div class="accordion-body">
-               text
+               {{ accordion.body }}
             </div>
          </div>
       </div>
@@ -21,15 +25,15 @@ export default {
       return {
          infoTabs: [
             {
-               titleItem: "How can we help?",
+               title: "How can we help?",
                body: "How many courses you've been on, using however many techniques and methods. Unless you've come to redlize the value of mindset, then you've yet to understand how success comes. With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success."
             },
             {
-               titleItem: "Why would I need a Business COACH?",
+               title: "Why would I need a Business COACH?",
                body: "How many courses you've been on, using however many techniques and methods. Unless you've come to redlize the value of mindset, then you've yet to understand how success comes. With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success."
             },
             {
-               titleItem: "What is one-on-one coaching?",
+               title: "What is one-on-one coaching?",
                body: "How many courses you've been on, using however many techniques and methods. Unless you've come to redlize the value of mindset, then you've yet to understand how success comes. With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success."
             }
          ]
