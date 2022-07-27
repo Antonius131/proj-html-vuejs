fe005bd8d1c0cd08a857e076a8d919e765fbef84
Antonius131 <antomendetta@gmail.com>
2022-07-27 11:09:43 +0200

    Buttons: creating new components

---
+++ src/components/main/PrimaryBtn.vue
@@ -0,0 +1,15 @@
+<template>
+   <button class="btn primary-btn">
+      Primary Btn
+   </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/main/SecondaryBtn.vue
@@ -0,0 +1,15 @@
+<template>
+   <button type="button" class="btn secondary-btn">
+      Secondary Btn
+   </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
