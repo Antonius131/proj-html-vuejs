9cd01ace658bf0d710e0be53a26e7c2da54e99b3
Antonius131 <antomendetta@gmail.com>
2022-07-27 21:22:38 +0200

    EventCards: creating component layout

--- src/components/main/EventCards.vue
+++ src/components/main/EventCards.vue
@@ -1,12 +1,32 @@
 <template>
    <div class="row">
-      
+      <div class="col-6">
+         <div class="card mb-3">
+            <div class="row g-0">
+               <div class="col-md-4">
+                  <h3>22</h3>
+                  <p class="card-text"><small>Month</small></p>
+                  <SecondaryBtn />
+               </div>
+               <div class="col-md-8">
+                  <div class="card-body">
+                     <p class="card-text"><small>City</small></p>
+                     <h4 class="card-title">Text</h4>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
    </div>
 </template>
 
 <script>
-export default {
+import SecondaryBtn from './SecondaryBtn.vue';
 
+export default {
+   components: { 
+      SecondaryBtn 
+   }
 }
 </script>
 
