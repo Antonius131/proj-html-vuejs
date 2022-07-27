70dde36135b0a053dbf2daed0cd35ff33be5af57
Antonius131 <antomendetta@gmail.com>
2022-07-27 21:27:49 +0200

    EventCArds: styling card

--- src/components/main/EventCards.vue
+++ src/components/main/EventCards.vue
@@ -3,16 +3,16 @@
       <div class="col-6">
          <div class="card mb-3">
             <div class="row g-0">
-               <div class="col-md-4">
-                  <h3>22</h3>
-                  <p class="card-text"><small>Month</small></p>
-                  <SecondaryBtn />
-               </div>
-               <div class="col-md-8">
+               <div class="col-md-9">
                   <div class="card-body">
                      <p class="card-text"><small>City</small></p>
                      <h4 class="card-title">Text</h4>
                   </div>
+               </div>
+               <div class="col-md-3 text-center">
+                  <h3 class="card-title">22</h3>
+                  <p class="card-text"><small>Month</small></p>
+                  <SecondaryBtn />
                </div>
             </div>
          </div>
@@ -30,6 +30,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../../assets/css/variables';
+
+   .card {
+      background-color: $bg_secondary;
+      border: none;
+   }
 
 </style>
