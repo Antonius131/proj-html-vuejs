7c5b49f52766a6df1c4f5c3a502ddc47fc78c425
Antonius131 <antomendetta@gmail.com>
2022-07-27 10:10:45 +0200

    BrandCards: creating component layout

--- src/components/main/BrandCards.vue
+++ src/components/main/BrandCards.vue
@@ -1,14 +1,26 @@
 <template>
    <div class="row">
-      <div class="card">
-         
+      <div class="col-4">
+         <div class="card mb-3">
+            <div class="row g-0">
+               <div class="col-md-4">
+                  <i>icon</i>
+               </div>
+               <div class="col-md-8">
+                  <div class="card-body">
+                     <h5 class="card-title">Title</h5>
+                     <p class="card-text">Subtitle</p>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
    </div>
 </template>
 
 <script>
 export default {
-
+   
 }
 </script>
 
