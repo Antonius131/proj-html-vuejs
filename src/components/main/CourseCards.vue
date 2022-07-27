e5ae72406bc05481455b97121a71a0b130d1c889
Antonius131 <antomendetta@gmail.com>
2022-07-27 16:18:20 +0200

    CourseCards: dynamically creating cards

--- src/components/main/CourseCards.vue
+++ src/components/main/CourseCards.vue
@@ -1,11 +1,15 @@
 <template>
    <div class="row">
-      <div class="col-3">
+      <div 
+         class="col-3"
+         v-for="(course, index) in coursesCards"
+         :key="index"
+      >
          <div class="card rounded-0">
-            <img src="" class="card-img" alt="card-img">
+            <img :src="course.imgPath" class="card-img" alt="card-img">
             <div class="card-img-overlay">
-               <h5 class="card-title">Title</h5>
-               <p class="card-text">Text</p>
+               <h5 class="card-title">{{ course.price }}</h5>
+               <p class="card-text">{{ course.text }}</p>
             </div>
          </div>
       </div>
@@ -14,7 +18,32 @@
 
 <script>
 export default {
-
+   data: function() {
+      return {
+         coursesCards: [
+            {
+               imgPath: require('/src/assets/img/courses/course-02-443x600.jpg'),
+               price: '$40.00',
+               text: 'Learning to Write as a Professional Author'
+            },
+            {
+               imgPath: require('/src/assets/img/courses/stock-full-hd-03-443x600.jpg'),
+               price: 'Free',
+               text: 'Customer-centric Info-Tech Strategies'
+            },
+            {
+               imgPath: require('/src/assets/img/courses/stock-full-hd-04-443x600.jpg'),
+               price: '$19.00',
+               text: 'Open Programming Courses for Everyone: Python'
+            },
+            {
+               imgPath: require('/src/assets/img/courses/stock-full-hd-06-443x600.jpg'),
+               price: '$26.00',
+               text: 'Academic Listening and Note-taking'
+            }
+         ]
+      }
+   }
 }
 </script>
 
