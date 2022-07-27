cf66c6845759f6dee0ca85cdf024c9bfc1add3e3
Antonius131 <antomendetta@gmail.com>
2022-07-27 11:26:23 +0200

    Buttons: styiling components

--- src/components/main/PrimaryBtn.vue
+++ src/components/main/PrimaryBtn.vue
@@ -1,5 +1,5 @@
 <template>
-   <button class="btn primary-btn">
+   <button type="button" class="btn btn-primary">
       Primary Btn
    </button>
 </template>
@@ -10,6 +10,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+   @import '../../assets/css/variables';
 
+      .btn-primary {
+         background-color: $brand_secondary;
+         border-color: $brand_secondary;
+         font-size: $nav_items;
+         padding: .75rem 2.5rem;
+         margin: 0 .5rem;
+         font-weight: 800;
+
+         &:hover {
+            background-color: $brand_primary;
+            border-color: $brand_primary;
+         }
+      }
 </style>
--- src/components/main/SecondaryBtn.vue
+++ src/components/main/SecondaryBtn.vue
@@ -1,5 +1,5 @@
 <template>
-   <button type="button" class="btn secondary-btn">
+   <button type="button" class="btn btn-secondary">
       Secondary Btn
    </button>
 </template>
@@ -10,6 +10,23 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+   @import '../../assets/css/variables';
 
+      .btn-secondary {
+         color: $brand_secondary;
+         font-size: $nav_items;
+         background-color: transparent;
+         border-color: rgba(33,33,33,.2);
+         padding: .75rem 2.5rem;
+         margin: 0 .5rem;
+         font-weight: 800;
+
+
+         &:hover {
+            background-color: $brand_primary;
+            border-color: $brand_primary;
+            color: $text_secondary;
+         }
+      }
 </style>
