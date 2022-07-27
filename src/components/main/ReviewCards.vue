6e8ec7d68318a2d898a3ec3534cbadfc2afcb45c
Antonius131 <antomendetta@gmail.com>
2022-07-27 22:18:42 +0200

    ReviewCards: adding review user box to template

--- src/components/main/ReviewCards.vue
+++ src/components/main/ReviewCards.vue
@@ -3,8 +3,13 @@
       <div class="col-4">
          <div class="card">
             <div class="card-body">
-               <h5 class="card-title">Special title treatment</h5>
-               <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+               <h5 class="card-title">title</h5>
+               <p class="card-text">text</p>
+               <div class="user-review-box">
+                  <img class="rounded-circle" src="" alt="">
+                  <h6 class="card-title">Name</h6>
+                  <p><small>job</small></p>
+               </div>
             </div>
          </div>
       </div>
