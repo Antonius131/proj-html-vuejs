e74c5977a6e1ee7882cc7fa7d7ed6f4036a21667
Antonius131 <antomendetta@gmail.com>
2022-07-27 10:59:33 +0200

    Main:  importing title component and create props

--- src/components/main/MainContent.vue
+++ src/components/main/MainContent.vue
@@ -2,12 +2,10 @@
    <main>
       <section>
          <div class="container">
-            <div class="row">
-               <div class="col-12 text-center section-title">
-                  <h4>Let's dream big together</h4>
-                  <h3>We are pioneers of the digital approach, using leading-edge technology to simplify procedures and apply executive coaching in the new age of digitalization.</h3>
-               </div>
-            </div>
+            <SectionTitle 
+               subtitle = "Let's dream big together"
+               title= "We are pioneers of the digital approach, using leading-edge technology to simplify procedures and apply executive coaching in the new age of digitalization."
+            />
             <BrandCards />
          </div>
       </section>
@@ -16,27 +14,12 @@
 
 <script>
 import BrandCards from './BrandCards.vue'
+import SectionTitle from './SectionTitle.vue'
 
 export default {
    components: {
-      BrandCards
-   },
-   data: function() {
-      return {
-         sectionTitles: [
-            {
-               id: 1,
-               subtitle: 'Let\'s dream big together',
-               title: ''
-            },
-            {
-               id: 2,
-               subtitle: 'Let\'s dream big together',
-               title: 'Explore how can I help you'
-            },
-
-         ]
-      }
+      BrandCards,
+      SectionTitle
    }
 }
 </script>
--- src/components/main/SectionTitle.vue
+++ src/components/main/SectionTitle.vue
@@ -1,14 +1,18 @@
 <template>
    <div class="row">
       <div class="col-12 text-center section-title">
-         <h4>subtitle</h4>
-         <h3>title</h3>
+         <h4>Subtitle</h4>
+         <h3>Title</h3>
       </div>
    </div>
 </template>
 
 <script>
 export default {
+   props: {
+      'subtitle': String,
+      'title': String
+   }
 }
 </script>
 
