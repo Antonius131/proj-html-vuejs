4d0f30a23f324b124c616b2dc35bc83549d5e5fe
Antonius131 <antomendetta@gmail.com>
2022-07-27 17:07:43 +0200

    CaptionText: creating new component

---
+++ src/components/main/CaptionText.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="row">
+      <div class="col-12">
+         <p>Caption text <a href="#">Link</a></p>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
