951d4aaf014a75e1e16f46e8ed5db19db3dcdc02
Antonius131 <antomendetta@gmail.com>
2022-07-27 22:24:52 +0200

    ReveiwCards: creating objects array

--- src/components/main/ReviewCards.vue
+++ src/components/main/ReviewCards.vue
@@ -18,7 +18,36 @@
 
 <script>
 export default {
-
+   data: function() {
+      return {
+         reviewCards: [
+            {
+               title: 'It\'s a choice of quality for people with special needs.',
+               text: 'I\'m a very strict person so I require everything to be organized and neat. Then, I\'ll be able to make things right and shine. MaxCoach guys just got me.',
+               name: 'Florence Themes',
+               job: '/ Multimedia Admin',
+               imgPath: require('/src/assets/img/reviews/testimonial-avata-03.jpg'),
+               isActive: false
+            },
+            {
+               title: 'It\'s a choice of quality for people with special needs.',
+               text: 'I\'m a very strict person so I require everything to be organized and neat. Then, I\'ll be able to make things right and shine. MaxCoach guys just got me.',
+               name: 'Florence Themes',
+               job: '/ Multimedia Admin',
+               imgPath: require('/src/assets/img/reviews/testimonial-avata-02.jpg'),
+               isActive: true
+            },
+            {
+               title: 'It\'s a choice of quality for people with special needs.',
+               text: 'I\'m a very strict person so I require everything to be organized and neat. Then, I\'ll be able to make things right and shine. MaxCoach guys just got me.',
+               name: 'Florence Themes',
+               job: '/ Multimedia Admin',
+               imgPath: require('/src/assets/img/reviews/testimonial-avata-01.jpg'),
+               isActive: false
+            },
+         ]
+      }
+   }
 }
 </script>
 
