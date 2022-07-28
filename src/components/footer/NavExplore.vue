a594624af6944a76dfa666a38ce265f83880e0ad
Antonius131 <antomendetta@gmail.com>
2022-07-28 09:28:14 +0200

    NavExplore: creating dynamic nav

--- src/components/footer/NavExplore.vue
+++ src/components/footer/NavExplore.vue
@@ -3,9 +3,13 @@
       <div class="col-12">
          <h5>Explore</h5>
       </div>
-      <div class="col-12">
+      <div 
+         class="col-12"
+         v-for="(link, index) in exploreItems"
+         :key="index"
+         >
          <nav class="nav row flex-column" >
-            <a class="nav-link" href="#">link</a>
+            <a class="nav-link" :href="link.url">{{ link.text }}</a>
          </nav>
       </div>
    </div>
@@ -13,6 +17,36 @@
 
 <script>
 export default {
+   data: function() {
+      return {
+         exploreItems: [
+            {
+               url: '#',
+               text: 'Start Here'
+            },
+            {
+               url: '#',
+               text: 'Blog'
+            },
+            {
+               url: '#',
+               text: 'About us'
+            },
+            {
+               url: '#',
+               text: 'Success story'
+            },
+            {
+               url: '#',
+               text: 'Courses'
+            },
+            {
+               url: '#',
+               text: 'Contact us'
+            },
+         ]
+      }
+   }
 }
 </script>
 
