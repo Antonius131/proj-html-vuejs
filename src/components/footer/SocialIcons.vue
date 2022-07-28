e0341f90d6ecdf341cf6ba2a37e10617a7d407cb
Antonius131 <antomendetta@gmail.com>
2022-07-28 08:27:15 +0200

    SocialIcons: creating dynamic contant with array

--- src/components/footer/SocialIcons.vue
+++ src/components/footer/SocialIcons.vue
@@ -1,14 +1,33 @@
 <template>
    <div class="row">
       <div class="col-12">
-         <i class=""></i>
+         <div 
+            class="icon-box"
+            v-for="brand in socialIcons"
+            :key="brand"
+         >
+            <i 
+               class="fa-brands"
+               :class="brand"
+            >
+            </i>
+         </div>
       </div>
    </div>
 </template>
 
 <script>
 export default {
-
+   data: function() {
+      return {
+         socialIcons: [
+            'fa-facebook',
+            'fa-twitter',
+            'fa-instagram',
+            'fa-linkedin'
+         ]
+      }
+   }
 }
 </script>
 
