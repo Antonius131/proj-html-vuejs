59a2f55ac8dabfdb9a137e8eb642897a10bb9fc4
Antonius131 <antomendetta@gmail.com>
2022-07-28 14:31:48 +0200

    clean Main template creating NewsletteSection.vue

--- src/components/main/MainContent.vue
+++ src/components/main/MainContent.vue
@@ -6,42 +6,29 @@
       <YtSection />
       <EventSection />
       <ReviewSection />
-
-      
-
-      <section class="newsletter-section">
-         <div class="container">
-            <div class="row">
-               <div class="col-6 offset-3 text-center">
-                  <h3><strong>Subscribe</strong> Newsletters</h3>
-                  <p>Enter your email address to register to our new newsletter subscription delivered on a regular basis!</p>
-               </div>
-            </div>
-            <InputText />
-         </div>
-      </section>
+      <NewsletterSection /> 
    </main>
 </template>
 
 <script>
-import InputText from './InputText.vue'
 import BrandSection from './BrandSection/BrandSection.vue'
 import CoachesSection from './CoachesSection/CoachesSection.vue'
 import CoursesSection from './CoursesSection/CoursesSection.vue'
 import YtSection from './YtSection/YtSection.vue'
 import EventSection from './EventSection/EventSection.vue'
 import ReviewSection from './ReviewSection/ReviewSection.vue'
+import NewsletterSection from './NewsletterSection/NewsletterSection.vue'
 
 export default {
    components: {
-    InputText,
-    BrandSection,
-    CoachesSection,
-    CoursesSection,
-    YtSection,
-    EventSection,
-    ReviewSection
-}
+      BrandSection,
+      CoachesSection,
+      CoursesSection,
+      YtSection,
+      EventSection,
+      ReviewSection,
+      NewsletterSection
+   }
 }
 </script>
 
@@ -54,24 +41,5 @@ export default {
 
    .secondary-bg {
       background-color: $border_color;
-   }
-
-   .newsletter-section {
-
-      h3 {
-         color: $brand_secondary;
-         font-weight: 400;
-
-         strong {
-            color: $text_primary;
-         }
-      }
-
-      .container {
-         background-image: url('/src/assets/img/shapes/maxcoach-shape-09.png');
-         background-repeat: no-repeat;
-         background-position: right;
-         background-size: 18%;
-      }
    }
 </style>
---
+++ src/components/main/NewsletterSection/NewsletterSection.vue
@@ -0,0 +1,43 @@
+<template>
+   <section class="newsletter-section">
+      <div class="container">
+         <div class="row">
+            <div class="col-6 offset-3 text-center">
+               <h3><strong>Subscribe</strong> Newsletters</h3>
+               <p>Enter your email address to register to our new newsletter subscription delivered on a regular basis!</p>
+            </div>
+         </div>
+         <InputText />
+      </div>
+   </section>
+</template>
+
+<script>
+import InputText from './InputText.vue';
+export default {
+    components: { InputText }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '/src/assets/css/variables';
+
+   .newsletter-section {
+
+      h3 {
+         color: $brand_secondary;
+         font-weight: 400;
+
+         strong {
+            color: $text_primary;
+         }
+      }
+
+      .container {
+         background-image: url('/src/assets/img/shapes/maxcoach-shape-09.png');
+         background-repeat: no-repeat;
+         background-position: right;
+         background-size: 18%;
+      }
+   }
+</style>
