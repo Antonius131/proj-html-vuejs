5b390f9d3f43e8529b5d0096316e34ee22a0befa
Antonius131 <antomendetta@gmail.com>
2022-07-28 07:17:39 +0200

    InputeText: creating component

---
+++ src/components/main/InputText.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="row">
+      <div class="input-group mb-3">
+         <input 
+            type="text"
+            class="form-control" 
+            placeholder="Enter your email" 
+         >
+         <button type="button">Subscribe</button>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
