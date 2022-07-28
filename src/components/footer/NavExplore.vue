aaa86c1aa8d91b404a5302900d66a143ca9ca0b6
Antonius131 <antomendetta@gmail.com>
2022-07-28 09:27:22 +0200

    NavExplore: creating component

---
+++ src/components/footer/NavExplore.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="row">
+      <div class="col-12">
+         <h5>Explore</h5>
+      </div>
+      <div class="col-12">
+         <nav class="nav row flex-column" >
+            <a class="nav-link" href="#">link</a>
+         </nav>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
