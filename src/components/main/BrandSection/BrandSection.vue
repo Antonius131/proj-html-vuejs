9175015d1cfab402f8227ae9d8afd41b46bb7fc1
Antonius131 <antomendetta@gmail.com>
2022-07-28 13:52:47 +0200

    clean Main <template> and create BrandSection.vue

---
+++ src/components/main/BrandSection/BrandSection.vue
@@ -0,0 +1,36 @@
+<template>
+   <section>
+      <div class="container">
+         <SectionTitle 
+            subtitle = "Let's dream big together"
+            title= "We are pioneers of the digital approach, using leading-edge technology to simplify procedures and apply executive coaching in the new age of digitalization."
+         />
+         <BrandCards />
+         <div class="row">
+            <div class="col-12 text-center">
+               <SecondaryBtn 
+                  text = "Learn more"
+               />
+               <PrimaryBtn 
+                  text = "Get started today"
+               />
+            </div>
+         </div>
+      </div>
+   </section>
+</template>
+
+<script>
+import BrandCards from '../BrandCards.vue';
+import SectionTitle from '../SectionTitle.vue';
+import PrimaryBtn from '../PrimaryBtn.vue';
+import SecondaryBtn from '../SecondaryBtn.vue';
+
+export default {
+    components: { BrandCards, SectionTitle, PrimaryBtn, SecondaryBtn }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/main/MainContent.vue
+++ src/components/main/MainContent.vue
@@ -1,24 +1,6 @@
 <template>
    <main>
-      <section>
-         <div class="container">
-            <SectionTitle 
-               subtitle = "Let's dream big together"
-               title= "We are pioneers of the digital approach, using leading-edge technology to simplify procedures and apply executive coaching in the new age of digitalization."
-            />
-            <BrandCards />
-            <div class="row">
-               <div class="col-12 text-center">
-                  <SecondaryBtn 
-                     text = "Learn more"   
-                  />
-                  <PrimaryBtn 
-                     text = "Get started today"
-                  />
-               </div>
-            </div>
-         </div>
-      </section>
+      <BrandSection />
 
       <section class="secondary-bg">
          <div class="container">
@@ -97,10 +79,7 @@
 </template>
 
 <script>
-import BrandCards from './BrandCards.vue'
 import SectionTitle from './SectionTitle.vue'
-import SecondaryBtn from './SecondaryBtn.vue'
-import PrimaryBtn from './PrimaryBtn.vue'
 import CoachCards from './CoachCards.vue'
 import CourseCards from './CourseCards.vue'
 import CaptionText from './CaptionText.vue'
@@ -109,13 +88,11 @@ import ImgPreview from './ImgPreview.vue'
 import EventCards from './EventCards.vue'
 import ReviewCards from './ReviewCards.vue'
 import InputText from './InputText.vue'
+import BrandSection from './BrandSection/BrandSection.vue'
 
 export default {
    components: {
-    BrandCards,
     SectionTitle,
-    SecondaryBtn,
-    PrimaryBtn,
     CoachCards,
     CourseCards,
     CaptionText,
@@ -123,7 +100,8 @@ export default {
     ImgPreview,
     EventCards,
     ReviewCards,
-    InputText
+    InputText,
+    BrandSection
 }
 }
 </script>
