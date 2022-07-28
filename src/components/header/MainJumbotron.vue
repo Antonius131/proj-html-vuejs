bae57f6a548b5e0c8a60be49653dc44647eb08b2
Antonius131 <antomendetta@gmail.com>
2022-07-28 13:41:41 +0200

    Jumbotron: adjusting row padding

--- src/components/header/MainJumbotron.vue
+++ src/components/header/MainJumbotron.vue
@@ -1,27 +1,25 @@
 <template>
-   <div class="row">
-      <div class="jumbotron">
-         <div class="container">
-            <div class="row align-items-center">
-               <div class="col-6 title-wrap">
-                  <h5>
-                     New Challenges, New Skills
-                  </h5>
-                  <h1>
-                     Build your own life choaching business
-                  </h1>
-                  <p>
-                     Whole-Life Business Coaching for committed entrepreneurs
-                  </p>
-                  <PrimaryBtn 
-                     text = "Get started today"
-                  />
-               </div>
-               <div class="col-6 img-wrap">
-                  <img class="img-fluid globe-img" src="../../assets/img/hero/home-business-hero-global-image.png" alt="globe-img">
-                  <img class="img-fluid avatar-img" src="../../assets/img/hero/home-business-hero-avatar.png" alt="avatar-img">
-               </div>            
+   <div class="jumbotron">
+      <div class="container">
+         <div class="row align-items-center">
+            <div class="col-6 title-wrap">
+               <h5>
+                  New Challenges, New Skills
+               </h5>
+               <h2>
+                  Build your own life choaching business
+               </h2>
+               <p>
+                  Whole-Life Business Coaching for committed entrepreneurs
+               </p>
+               <PrimaryBtn 
+                  text = "Get started today"
+               />
             </div>
+            <div class="col-6 img-wrap">
+               <img class="img-fluid globe-img" src="../../assets/img/hero/home-business-hero-global-image.png" alt="globe-img">
+               <img class="img-fluid avatar-img" src="../../assets/img/hero/home-business-hero-avatar.png" alt="avatar-img">
+            </div>            
          </div>
       </div>
    </div>
@@ -40,46 +38,47 @@ export default {
 <style lang="scss" scoped>
    @import '../../assets/css/variables.scss';
 
-   .row {
-      padding: 0;
+   .jumbotron {
+      background-color: $hero_bg;
 
-      .jumbotron {
-         background-color: $hero_bg;
+      .row {
+         padding-top: 2rem;
+         padding-bottom: 0;
+      }
 
-         .title-wrap {
-            padding-right: 6rem;
+      .title-wrap {
+         padding-right: 6rem;
 
-            h5 {
-               color: $brand_secondary;
-               font-weight: 00;
-               margin-bottom: 1rem;
-            }
-
-            h1 {
-               color: $brand_primary;
-               text-transform: uppercase;
-               font-weight: 800;
-            }
-
-            p {
-               color: $text_primary_rgba;
-            }
+         h5 {
+            color: $brand_secondary;
+            font-weight: 00;
+            margin-bottom: 1rem;
          }
 
-         .img-wrap {
-            position: relative;
-            line-height: 0;
+         h2 {
+            color: $brand_primary;
+            text-transform: uppercase;
+            font-weight: 800;
+         }
 
-            .globe-img {
-               transform: rotate(-45deg);
-            }
+         p {
+            color: $text_primary_rgba;
+         }
+      }
 
-            .avatar-img {
-               position: absolute;
-               left: 0;
-               bottom: 0;
-               height: 100%;
-            }
+      .img-wrap {
+         position: relative;
+         line-height: 0;
+
+         .globe-img {
+            transform: rotate(-45deg);
+         }
+
+         .avatar-img {
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            height: 100%;
          }
       }
    }
