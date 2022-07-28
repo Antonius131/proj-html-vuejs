4e4d09e58ffdbd6a90b24184a3899f23cbcb83b4
Antonius131 <antomendetta@gmail.com>
2022-07-28 08:14:00 +0200

    SocialIcons: creating component

---
+++ src/components/footer/SocialIcons.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="row">
+      <div class="col-12">
+         <i class=""></i>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
