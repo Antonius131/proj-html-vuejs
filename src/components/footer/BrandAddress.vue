3a7388debed12cc1af382607dc73984913822950
Antonius131 <antomendetta@gmail.com>
2022-07-28 08:34:43 +0200

    BrandAddress: creating component

---
+++ src/components/footer/BrandAddress.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="row">
+      <div class="col-12">
+         <h5>Address</h5>
+         <p>382 NE 191st St # 87394 Miami, FL 33179-3899</p>
+         <p>+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)</p>
+         <a href="mailto:support@maxcoach.com">support@maxcoach.com</a>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
