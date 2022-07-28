d015828b0f8f824d7d7150cc484f0864d276500d
Antonius131 <antomendetta@gmail.com>
2022-07-28 09:40:13 +0200

    NavInfo: creating component

---
+++ src/components/footer/NavInfo.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="row">
+      <div class="col-12">
+         <h5>Explore</h5>
+      </div>
+      <div class="col-12">
+         <nav class="nav row flex-column">
+            <ul>
+               <li>
+                  <a class="nav-link" href="#">Link</a>
+               </li>
+            </ul>
+         </nav>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
