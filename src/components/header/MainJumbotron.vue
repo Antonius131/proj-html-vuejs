85f7b75e42fede11e7a9d99cf2261b14b2b0229b
Antonius131 <antomendetta@gmail.com>
2022-07-28 12:05:22 +0200

    Jumbotron: adding images

--- src/components/header/MainJumbotron.vue
+++ src/components/header/MainJumbotron.vue
@@ -1,17 +1,20 @@
 <template>
-   <div class="jumbotron">
-      <div class="container">
-         <div class="row">
-            <div class="col-6 wrap title-wrap">
-               <SectionTitle 
-                  subtitle = "New Challenges, New Skills"
-                  title = "Build your own life choaching business"
-                  paragraph = "Whole-Life Business Coaching for committed entrepreneurs"
-               />
+   <div class="row">
+      <div class="jumbotron">
+         <div class="container">
+            <div class="row">
+               <div class="col-6 wrap title-wrap">
+                  <SectionTitle 
+                     subtitle = "New Challenges, New Skills"
+                     title = "Build your own life choaching business"
+                     paragraph = "Whole-Life Business Coaching for committed entrepreneurs"
+                  />
+               </div>
+               <div class="col-6 img-wrap">
+                  <img class="img-fluid globe-img" src="../../assets/img/hero/home-business-hero-global-image.png" alt="globe-img">
+                  <img class="img-fluid avatar-img" src="../../assets/img/hero/home-business-hero-avatar.png" alt="avatar-img">
+               </div>            
             </div>
-            <div class="col-6 wrap img-wrap">
-               <!-- <img src="../../assets/img/hero/home-business-hero-avatar.png" alt="avatar"> -->
-            </div>            
          </div>
       </div>
    </div>
@@ -28,7 +31,27 @@ export default {
 <style lang="scss" scoped>
    @import '../../assets/css/variables.scss';
 
-   .jumbotron {
-      background-color: $hero_bg;
+   .row {
+      padding: 0;
+
+      .jumbotron {
+         background-color: $hero_bg;
+
+         .img-wrap {
+            position: relative;
+            line-height: 0;
+
+            .globe-img {
+               transform: rotate(-45deg);
+            }
+
+            .avatar-img {
+               position: absolute;
+               left: 0;
+               bottom: 0;
+               height: 100%;
+            }
+         }
+      }
    }
 </style>
